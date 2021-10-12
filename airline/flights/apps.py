a7e6f803831c08490938e9f8b9cc8f7af87c4701
fmstam <faroq.al.tam@gmail.com>
2021-10-12 09:41:43 +0100

    refac

--- airline/flights/apps.py
+++ airline/flights/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FightsConfig(AppConfig):
+class FlightsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fights'
+    name = 'flights'

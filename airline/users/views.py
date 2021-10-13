e7bea6e0628a903f54dc269e4a2a9789fe17d508
fmstam <faroq.al.tam@gmail.com>
2021-10-13 15:32:28 +0100

    working on the project

--- airline/users/views.py
+++ airline/users/views.py
@@ -1,3 +1,15 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpRequest, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
+
+def index(request:HttpRequest)-> HttpResponse:
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+def login(request:HttpRequest)-> HttpResponse:
+    return render(request, "users/login.html")
+
+def logout(request:HttpRequest)-> HttpResponse:
+    pass

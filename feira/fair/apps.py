fb6a04828e58f391b2d0f5882f11f33e89ccd268
fmstam <faroq.al.tam@gmail.com>
2021-10-13 16:39:01 +0100

    changing name

--- feira/fair/apps.py
+++ feira/fair/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FairbidConfig(AppConfig):
+class FairConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fairbid'
+    name = 'fair'

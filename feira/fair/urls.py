31612593bb06af049c574324043c2b7e701e0eff
fmstam <faroq.al.tam@gmail.com>
2021-10-19 10:44:39 +0100

    edit-login rediction is working

--- feira/fair/urls.py
+++ feira/fair/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from django.views.generic.base import TemplateView
-from .views import ListingView, ListingCreateView
+from .views import ListingView, ListingCreateView, ListingUpdateView
 
 app_name ='fair'
 urlpatterns =[
@@ -9,6 +9,6 @@ urlpatterns =[
     path('listings/', ListingView.as_view(), name='listings'),
     path('listings/user/<int:owner_id>', ListingView.as_view(), name='user_listings'),
     path('listings/view/<int:listing_id>', ListingView.as_view(), name='view_a_listing'),
-    path('listings/edit/<int:listing_id>', ListingView.as_view(), name='edit_a_listing'),
+    path('listings/edit/<int:listing_id>', ListingUpdateView.as_view(), name='edit_a_listing'),
     path('listings/new/', ListingCreateView.as_view(), name='create_a_listing')
 ]
--- feira/fair/views.py
+++ feira/fair/views.py
@@ -1,12 +1,14 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .forms import ListingForm
 from .models import Listing
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
+# view class
 class ListingView(CreateView):
     form_class = ListingForm
     template_name = 'fair/listings.html'
@@ -24,13 +26,42 @@ class ListingView(CreateView):
                      )
 
 
-class ListingCreateView(CreateView):
+# create new class
+class ListingCreateView(LoginRequiredMixin, CreateView):
+    form_class = ListingForm
+    template_name = 'fair/new_listing.html'
+    
+    def get_success_url(self, **kwargs) -> str:
+        """instead of using reverse_lazy"""
+        return reverse('home')
+    
+    def get_login_url(self) -> str:
+        """
+        Make sure the user is logged in
+        """
+        return reverse('accounts:login')
+
+    def form_valid(self, form: ListingForm) -> HttpResponse:
+        self.object = form.save(commit=False)
+        self.object.owner = self.request.user # set the fk
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+
+# edit class
+class ListingUpdateView(LoginRequiredMixin, UpdateView):
     form_class = ListingForm
     template_name = 'fair/new_listing.html'
 
     def get_success_url(self, **kwargs) -> str:
         """instead of using reverse_lazy"""
         return reverse('home')
+
+    def get_login_url(self) -> str:
+        """
+        Make sure the user is logged in
+        """
+        return reverse('accounts:login')
 
     def form_valid(self, form: ListingForm) -> HttpResponse:
         self.object = form.save(commit=False)
